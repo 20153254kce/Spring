b9d8c238da417bbcb3598dc433145bdfa673c5ac
20153254kce <kceuni0405@gmail.com>
2026-06-15 12:20:53 +0900

    .

---
+++ ch02/src/main/java/org/example/advice/MyAdvice.java
@@ -0,0 +1,42 @@
+package org.example.advice;
+
+import org.aspectj.lang.annotation.*;
+import org.springframework.stereotype.Component;
+
+@Aspect //부가기능을 수행하는 Aspect 모듈 선언
+@Component
+public class MyAdvice {
+
+    //포인트컷 설정, 부가기능이 실행될 조건 설정, 내용이 없는 매서드
+    @Pointcut("execution(void org.example.service.ArticleService.register())")
+    public void insertPointCut() {}
+
+    @Pointcut("execution(void org.example.service.ArticleService.get*(..))")
+    public void selectPointCut() {}
+
+
+
+    // 부가기능 매서드(어드바이스)
+    @Before("insertPointCut() || selectPointCut()")
+    public void before() {
+        System.out.println("-----------------------------------");
+        System.out.println("부가기능 - before...");
+    }
+
+    @After("insertPointCut() || selectPointCut()")
+    public void after() {
+        System.out.println("부가기능 - after...");
+        System.out.println("-----------------------------------");
+    }
+
+   @AfterReturning("insertPointCut()")
+    public void afterReturn() {
+        System.out.println("부가기능 - afterReturn...");
+    }
+
+    @AfterReturning("selectPointCut()")
+    public void afterThrow() {
+        System.out.println("부가기능 - afterThrow...");
+        System.out.println("-----------------------------------");
+    }
+}
